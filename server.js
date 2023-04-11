4a8ba32881b4962f2319677fe95f328d903ec523
vooshadmin <74595150+vooshadmin@users.noreply.github.com>
2023-04-11 14:39:32 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.post("/", (req, res) => {
+  return res.json({ message: "Hello World!", data: req?.body });
+});
+
+app.listen(1300, () => console.log(`Server running on port: 1300`));
